b7d1cf511657400cac161822473e88a4abf98406
ishan sapkota <ishansapkota46@gmail.com>
2023-11-23 21:05:11 +0545

    This is the first commit in the last year projects new repo which includes a server that is running. Other additions to the server will me made on a later date

--- app.js
+++ app.js
@@ -2,3 +2,14 @@
     Entry point for the server!
     Happy Coding🧑‍💻🥰
 */
+console.log("This is the server of our project🥱🥺");
+
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`The server is running on port ${port}`);
+});
