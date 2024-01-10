257f673e123e532eb0054c2177880cb3507dc7e8
ishan sapkota <ishansapkota46@gmail.com>
2024-01-10 09:53:54 +0545

    Flutter App Created

---
+++ client/p2p/lib/constants/color_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const primaryColor = Colors.blue;
+  static const secondaryColor = Colors.yellow;
+  static const navBarColor = Colors.accents;
+}
